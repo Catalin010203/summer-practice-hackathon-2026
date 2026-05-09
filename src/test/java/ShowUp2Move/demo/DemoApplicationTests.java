c1d2f509893cb165ceb90250c1ddda936b66bb9e
Sandu Catalin <catalinsandu697@gmail.com>
2026-05-09 10:23:35 +0300

    Initial commit

---
+++ src/test/java/ShowUp2Move/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ShowUp2Move.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
